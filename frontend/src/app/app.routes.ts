60847bc9c94e2b9d263e3f858b73052e1c693f35
Andrea C <an9rya@gmail.com>
2026-05-15 21:15:19 -0400

    Add Group component with routing, styling, and Bootstrap integration

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Group } from './component/group/group';
+import { Map } from './component/map/map';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', component: Map },
+	{ path: 'group', component: Group },
+];
---
+++ frontend/src/app/component/group/group.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-group',
+  imports: [],
+  templateUrl: './group.html',
+  styleUrl: './group.scss'
+})
+export class Group {}
